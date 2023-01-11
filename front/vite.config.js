c2e06f6e5743221ff027b36a1f36f9106a8e1e4a
rgsone <rgs@rgsone.com>
2023-01-11 21:54:05 +0100

    update vite config

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,42 +1,50 @@
-import { defineConfig } from "vite";
+import {defineConfig, loadEnv} from "vite";
 
-export default defineConfig({
+export default defineConfig(({command, mode }) => {
 
-	root: './src',
-	//base: '/assets/',
+	//console.log(loadEnv(mode, process.cwd(), 'PUBLIC_'));
 
-	server: {
-		port: 5173,
-		https: false,
-		//host: 'brm-calendar.local',
-		cors: {
-			origin: '*',
-			methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+	return {
+
+		root: './src',
+		//base: '/assets/',
+		envDir: process.cwd(),
+		envPrefix: 'PUBLIC_',
+
+		server: {
+			port: 5173,
+			https: false,
+			//host: 'brm-calendar.local',
+			cors: {
+				origin: '*',
+				methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+			},
+			proxy: {
+				'/api': {
+					target: 'http://rgsone.local:8888',
+					changeOrigin: true,
+					secure: false,
+					rewrite: (path) => path.replace(/^\/api/, '')
+				}
+			}
 		},
-		proxy: {
-			'/api': {
-				target: 'http://rgsone.local:8888',
-				changeOrigin: true,
-				secure: false,
-				rewrite: (path) => path.replace(/^\/api/, '')
-			}
-		}
-	},
 
-	build: {
-		//manifest: true,
-		emptyOutDir: false,
-		outDir: '../../public',
-		cssCodeSplit: false,
-		//assetsDir: 'assets', // won't work because output in rollup is defined
-		rollupOptions: {
-			input: './src/main.js',
-			output: {
-				entryFileNames: 'assets/[name].js',
-				chunkFileNames: 'assets/[name].js',
-				assetFileNames: 'assets/[name].[ext]'
+		build: {
+			//manifest: true,
+			emptyOutDir: false,
+			outDir: '../../public',
+			cssCodeSplit: false,
+			//assetsDir: 'assets', // won't work because output in rollup is defined
+			rollupOptions: {
+				input: './src/main.js',
+				output: {
+					entryFileNames: 'assets/[name].js',
+					chunkFileNames: 'assets/[name].js',
+					assetFileNames: 'assets/[name].[ext]'
+				}
 			}
 		}
+
 	}
 
 });
