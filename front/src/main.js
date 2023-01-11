afb1a3a7ec3faf3fda237a0d9d332a948e2401dc
rgsone <rgs@rgsone.com>
2023-01-11 21:58:39 +0100

    update front

--- front/src/main.js
+++ front/src/main.js
@@ -8,26 +8,80 @@ document.addEventListener('alpine:init', () => {
 	//// STORE ////
 
 	Alpine.store('brmData', {
-		hasData: false,
-		data: null,
-		filterBtnText: 'Filtrer',
-		isFilterBtnDisabled: false,
+		brmData: [],
+		paginationData: null,
+		formData: {
+			action: 'getData',
+			distance: '',
+			month: '',
+			country: '',
+			page: ''
+		},
+		isLoadingData: false,
+		fetchError: false,
 
-		init() {
-			this.filterBtnText = '•••';
-			this.isFilterBtnDisabled = true;
-			const data = this.fetchData({
+		init() { },
 
+		async submitForm() {
+			if (this.isLoadingData) return;
+
+			this.isLoadingData = true;
+			this.fetchError = false;
+
+			// oneliner timer :) for better smooth transition between states
+			await new Promise(resolve => setTimeout(resolve, 1500));
+
+			const resData = this.fetchData(this.formData);
+
+			resData.then((data) => {
+				if (data.isError) {
+					this.fetchError = true;
+				} else {
+					this.updateListData(data);
+				}
+				this.isLoadingData = false;
 			});
-			this.updateListData(data);
 		},
 
 		async fetchData(formData) {
-			//const response = await fetch()
+			const res = await fetch(import.meta.env.PUBLIC_API_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: new URLSearchParams(formData)
+			});
+
+			if (res.status !== 200 || !res.ok) {
+				return {
+					isError: true,
+					pagination: null,
+					data: null,
+				};
+			}
+
+			let content = null;
+
+			try {
+				content = await res.json();
+			} catch (e) {
+				return {
+					isError: true,
+					pagination: null,
+					data: null,
+				};
+			}
+
+			return {
+				isError: false,
+				pagination: content.pagination,
+				data: content.data,
+			};
 		},
 
 		updateListData(data) {
-			// handle dist data
+			this.brmData = (data.data == null) ? [] : data.data;
+			this.paginationData = data.pagination;
 		}
 	});
 
@@ -35,32 +89,114 @@ document.addEventListener('alpine:init', () => {
 
 	Alpine.data('filterFormHandler', () => ({
 
-		formData: {
-			distance: '',
-			month: '',
-			country: ''
-		},
-
 		submit() {
-			console.log(this.formData);
-
-			// test if data are defined
-			if (this.formData.distance === '' || this.formData.month === '' || this.formData.country === '') return;
-
-			console.log('send data');
+			Alpine.store('brmData').formData.page = '';
+			Alpine.store('brmData').submitForm();
 		},
 
 		get isSubmitBtnDisabled() {
-			return this.formData.distance === '' || this.formData.month === '' || this.formData.country === '' || Alpine.store('brmData').isFilterBtnDisabled;
+			const store = Alpine.store('brmData');
+			return store.formData.distance === '' ||
+				store.formData.month === '' ||
+				store.formData.country === '' ||
+				store.isLoadingData;
+		},
+
+		get filterBtnTxt() {
+			return Alpine.store('brmData').isLoadingData ? '•••' : 'Filtrer';
 		}
 
 	}));
 
 	Alpine.data('brmList', () => ({
+
 		init() {
 
+		},
+
+		nextPage() {
+			const store = Alpine.store('brmData');
+			if (store.paginationData.pages.nextPageNumber === null) return;
+			store.formData.page = store.paginationData.pages.nextPageNumber;
+			store.submitForm();
+		},
+
+		lastPage() {
+			const store = Alpine.store('brmData');
+			if (store.paginationData.pages.isLastPage) return;
+			store.formData.page = store.paginationData.pages.lastPageNumber;
+			store.submitForm();
+		},
+
+		prevPage() {
+			const store = Alpine.store('brmData');
+			if (store.paginationData.pages.prevPageNumber === null) return;
+			store.formData.page = store.paginationData.pages.prevPageNumber;
+			store.submitForm();
+		},
+
+		firstPage() {
+			const store = Alpine.store('brmData');
+			if (store.paginationData.pages.isFirstPage) return;
+			store.formData.page = store.paginationData.pages.firstPageNumber;
+			store.submitForm();
+		},
+
+		getTextContent(data) {
+			const elevation = data.elevation > 0 ? `/ ${data.elevation} D+` : '';
+			return `Debra BANKS (rba@davisbikeclub.org)
+							<br>
+							club -> Davis Bike Club
+							<br>
+							USA / CA / Departement / Davis
+							<br>
+							site : https://www.davisbikeclub.org/ultra-distance-brevets-and-randonneuring
+							<br>
+							trace :`;
+		},
+
+		formatDistance(data) { return `${data} km`; },
+
+		distanceClass(data) {
+			switch (data) {
+				case 200:
+					return 'bg-amber-400';
+				case 300:
+					return 'bg-amber-500';
+				case 400:
+					return 'bg-amber-600';
+				case 600:
+					return 'bg-amber-700';
+				case 1000:
+					return 'bg-amber-800';
+				default:
+					return 'bg-amber-400';
+			}
+		},
+
+		formatElevation(data) { return `${data} d+`; },
+
+		formatContact(data) { return `${data.contact} (${data.contact_mail})`; },
+
+		get paginationInfoText() {
+			const store = Alpine.store('brmData');
+
+			if (store.paginationData == null) return '';
+			if (store.brmData.length === 0) return '0 - 0 / 0';
+
+			return store.paginationData.items.pageFirstItemIndex +
+				' - ' + store.paginationData.items.pageLastItemIndex +
+				' / ' + store.paginationData.items.totalItems;
 		}
+
 	}));
+
+	//// LAUNCH ////
+
+	Alpine.store('brmData').formData.distance = 0;
+	Alpine.store('brmData').formData.month = 0;
+	Alpine.store('brmData').formData.country = 'world';
+	Alpine.store('brmData').submitForm();
 
 });
 
--- view/home.php
+++ view/home.php
@@ -4,10 +4,6 @@
 	use Rgsone\BrmCalendar\Month;
 	use Rgsone\BrmCalendar\Country;
 
-	require dirname(__DIR__).'/api/Distance.php';
-	require dirname(__DIR__).'/api/Month.php';
-	require dirname(__DIR__).'/api/Country.php';
-
 ?>
 <!doctype html>
 <html lang="fr">
@@ -17,7 +13,9 @@
 		<title>Calendrier BRM 2023</title>
 		<meta name="description" content="Calendrier des BRM 2023">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- detect host and add/remove style tag -->
+		<?php if (!$isDevEnv): ?>
+		<link rel="stylesheet" href="/assets/style.css">
+		<?php endif; ?>
 	</head>
 
 	<body class="bg-slate-100 flex flex-col">
@@ -30,7 +28,7 @@
 			<div class="flex flex-col sm:flex-row sm:mb-3 md:mb-0">
 
 				<div class="flex flex-col mb-4 sm:mb-0 sm:mr-2">
-					<select name="distance" id="distance" x-model="formData.distance" class="rounded-md">
+					<select name="distance" id="distance" x-model="$store.brmData.formData.distance" class="rounded-md">
 							<option value="" disabled>Distance</option>
 			  		<?php foreach (Distance::cases() as $distance): ?>
 							<option value="<?= $distance->value ?>" <?php if ($distance === Distance::TWO_HUNDRED) echo 'selected="selected"'; ?>><?= $distance->frenchName() ?></option>
@@ -39,7 +37,7 @@
 				</div>
 
 				<div class="flex flex-col mb-4 sm:mb-0 sm:mr-2">
-					<select name="month" id="month" x-model="formData.month" class="rounded-md">
+					<select name="month" id="month" x-model="$store.brmData.formData.month" class="rounded-md">
 							<option value="" disabled>Mois</option>
 			  		<?php foreach (Month::cases() as $month): ?>
 							<option value="<?= $month->value ?>"><?= $month->frenchName() ?></option>
@@ -48,7 +46,7 @@
 				</div>
 
 				<div class="flex flex-col mb-4 sm:mb-0 sm:mr-2">
-					<select name="country" id="country" x-model="formData.country" class="rounded-md">
+					<select name="country" id="country" x-model="$store.brmData.formData.country" class="rounded-md">
 							<option value="" disabled>Pays</option>
 			  		<?php foreach (Country::cases() as $country): ?>
 							<option value="<?= $country->value ?>"><?= $country->frenchName() ?></option>
@@ -58,74 +56,110 @@
 
 			</div>
 
-			<button :disabled="isSubmitBtnDisabled" x-text="$store.brmData.filterBtnText" class="bg-indigo-500 disabled:bg-indigo-100 text-white px-8 py-2 rounded-md hover:bg-indigo-600 self-end sm:self-start md:self-auto">Filtrer</button>
+			<button :disabled="isSubmitBtnDisabled" x-text="filterBtnTxt" class="bg-indigo-500 disabled:bg-indigo-100 text-white px-8 py-2 rounded-md hover:bg-indigo-600 self-end sm:self-start md:self-auto">Filtrer</button>
 
 		</form>
 
-		<!-- results container -->
-		<div id="results" x-data="brmList" >
+		<!-- results list container -->
+		<div class="relative mx-auto max-w-5xl w-full">
 
-			<!-- top pagination-->
-			<div class="flex justify-between items-center p-3 max-w-5xl mx-auto w-full">
-
-				<div>
-					<button class="border-2 font-bold px-3 py-2 rounded-md border-indigo-300 text-indigo-500">&lt;&lt;</button>
-					<button class="border-2 font-bold px-4 py-2 rounded-md border-indigo-300 text-indigo-500">&lt;</button>
-				</div>
-
-				<div class="">
-					1-10 / 60
-				</div>
-
-				<div>
-					<button class="border-2 font-bold px-4 py-2 rounded-md border-indigo-300 text-indigo-500">&gt;</button>
-					<button class="border-2 font-bold px-3 py-2 rounded-md border-indigo-300 text-indigo-500">&gt;&gt;</button>
-				</div>
-
+			<!-- error container -->
+			<div x-data
+					 x-show="!$store.brmData.isLoadingData && $store.brmData.fetchError"
+					 x-transition:enter="transition origin-top ease-out duration-700"
+					 x-transition:enter-start="opacity-0"
+					 x-transition:enter-end="opacity-100"
+					 x-transition:leave="transition origin-top ease-out duration-500"
+					 x-transition:leave-start="opacity-100"
+					 x-transition:leave-end="opacity-0"
+					 class="p-3 w-full absolute">
+				<p class="bg-red-500 text-red-50 p-4 rounded-md">Erreur durant la récupération des données.</p>
 			</div>
 
-			<!-- results -->
-			<ul class="p-3 max-w-5xl mx-auto w-full">
-				<li class="bg-white shadow mb-4 px-4 py-5 last:mb-0">
-					01/01/2023 / 200 km / 1200 D+
-					<br>
-					Debra BANKS (rba@davisbikeclub.org)
-					<br>
-					club -> Davis Bike Club
-					<br>
-					USA / CA / Departement / Davis
-					<br>
-					site : https://www.davisbikeclub.org/ultra-distance-brevets-and-randonneuring
-					<br>
-					trace :
-				</li>
-			</ul>
+			<!-- loading container -->
+			<div x-data
+					 x-show="$store.brmData.isLoadingData"
+					 x-transition:enter="transition origin-top ease-out duration-700"
+					 x-transition:enter-start="transform -translate-y-12 opacity-0"
+					 x-transition:enter-end="transform translate-y-0 opacity-100"
+					 x-transition:leave="transition origin-top ease-out duration-500"
+					 x-transition:leave-start="transform translate-y-0 opacity-100"
+					 x-transition:leave-end="transform -translate-y-12 opacity-0"
+					 class="p-3 w-full absolute">
+				<p class="bg-white p-4 rounded-md">Récupération des données...</p>
+			</div>
 
-			<!-- bottom pagination -->
-			<div class="flex justify-between items-center p-4 mb-10 max-w-5xl mx-auto w-full">
+			<!-- results container -->
+			<div x-data="brmList"
+					 x-show="!$store.brmData.isLoadingData && !$store.brmData.fetchError"
+					 x-transition:enter="transition origin-top ease-out duration-700"
+					 x-transition:enter-start="transform translate-y-4 opacity-0"
+					 x-transition:enter-end="transform translate-y-0 opacity-100"
+					 x-transition:leave="transition origin-top ease-out duration-500"
+					 x-transition:leave-start="transform translate-y-0 opacity-100"
+					 x-transition:leave-end="transform translate-y-6 opacity-0"
+					 id="results">
 
-				<div>
-					<button class="border-2 font-bold px-3 py-2 rounded-md border-indigo-300 text-indigo-500">&lt;&lt;</button>
-					<button class="border-2 font-bold px-4 py-2 rounded-md border-indigo-300 text-indigo-500">&lt;</button>
+				<!-- top pagination-->
+				<div class="flex justify-between items-center p-3 w-full">
+
+					<div>
+						<button @click="firstPage" class="border-2 font-bold px-3 py-2 rounded-md border-indigo-300 text-indigo-500 hover:bg-indigo-300 hover:text-indigo-900">&lt;&lt;</button>
+						<button @click="prevPage" class="border-2 font-bold px-4 py-2 rounded-md border-indigo-300 text-indigo-500 hover:bg-indigo-300 hover:text-indigo-900">&lt;</button>
+					</div>
+
+					<div x-text="paginationInfoText" class="font-bold"></div>
+
+					<div>
+						<button @click="nextPage" class="border-2 font-bold px-4 py-2 rounded-md border-indigo-300 text-indigo-500 hover:bg-indigo-300 hover:text-indigo-900">&gt;</button>
+						<button @click="lastPage" class="border-2 font-bold px-3 py-2 rounded-md border-indigo-300 text-indigo-500 hover:bg-indigo-300 hover:text-indigo-900">&gt;&gt;</button>
+					</div>
+
 				</div>
 
-				<div class="">
-					1-10 / 60
-				</div>
+				<!-- results -->
+				<ul class="p-3 w-full">
+					<template x-for="brm in $store.brmData.brmData" :key="brm.id">
+						<li class="bg-white shadow mb-4 px-4 py-5 last:mb-0">
+							<div class="flex">
+								<div x-text="brm.date" class="bg-slate-500 text-white rounded px-2 py-1 text-sm mr-1"></div>
+								<div x-text="formatDistance(brm.distance)" :class="distanceClass(brm.distance)" class="text-white rounded px-2 py-1 text-sm mr-1"></div>
+								<template x-if="brm.elevation > 0">
+									<div x-text="formatElevation(brm.elevation)" class="px-2 py-1 text-sm"></div>
+								</template>
+							</div>
+							<p x-text="formatContact(brm)"></p>
+						</li>
+					</template>
+				</ul>
 
-				<div>
-					<button class="border-2 font-bold px-4 py-2 rounded-md border-indigo-300 text-indigo-500">&gt;</button>
-					<button class="border-2 font-bold px-3 py-2 rounded-md border-indigo-300 text-indigo-500">&gt;&gt;</button>
+				<!-- bottom pagination -->
+				<div class="flex justify-between items-center p-4 mb-10 w-full">
+
+					<div>
+						<button class="border-2 font-bold px-3 py-2 rounded-md border-indigo-300 text-indigo-500">&lt;&lt;</button>
+						<button class="border-2 font-bold px-4 py-2 rounded-md border-indigo-300 text-indigo-500">&lt;</button>
+					</div>
+
+					<div x-text="paginationInfoText" class="font-bold"></div>
+
+					<div>
+						<button class="border-2 font-bold px-4 py-2 rounded-md border-indigo-300 text-indigo-500">&gt;</button>
+						<button class="border-2 font-bold px-3 py-2 rounded-md border-indigo-300 text-indigo-500">&gt;&gt;</button>
+					</div>
+
 				</div>
 
 			</div>
 
 		</div>
 
-		<!--<script src="js/app.js"></script>-->
-		<!-- detect host and switch dev/prod script tag -->
-		<script type="module" src="http://localhost:5173/@vite/client"></script>
-		<script type="module" src="http://localhost:5173/main.js"></script>
+		<?php if ($isDevEnv): ?>
+			<script type="module" src="http://localhost:5173/@vite/client"></script>
+			<script type="module" src="http://localhost:5173/main.js"></script>
+		<?php else: ?>
+			<script src="/assets/main.js" defer></script>
+		<?php endif; ?>
 
 	</body>
 
